a945e68b1366e0af592ead6974202d16d8fd1003
Paul Josué Michel <d47303647@instituto.itr.edu.ar>
2023-08-24 16:29:59 -0300

    mazeRunner terminado

--- mazeRunner.js
+++ mazeRunner.js
@@ -1,20 +1,22 @@
 // Buscador de salidas de laberintos
 
-function wayFinder(x,y,maze){
-    if(maze[y] < 0 || maze[x] < 0 || maze[y] > maze.length || maze[x] > maze.length) return false;
-    if(maze[y][x] == "G") return true;
-    if(maze[y][x] == "#") return false;
-    if(maze[y][x] == "+") return false;
+function wayFinder(x, y, maze) {
+    if (y < 0 || x < 0 || y >= maze.length || x >= maze.length) return false;
+    if (maze[y][x] == "G") return true;
+    if (maze[y][x] == "#") return false;
+    if (maze[y][x] == "+") return false;
     maze[y][x] = "+";
 
-    if(wayFinder(x,y-1,maze) == true) return true;
-    if(wayFinder(x+1,y,maze) == true) return true;
-    if(wayFinder(x,y+1,maze) == true) return true;
-    if(wayFinder(x-1,y,maze) == true) return true;
+    if (wayFinder(x, y - 1, maze) == true) return true;
+    if (wayFinder(x + 1, y, maze) == true) return true;
+    if (wayFinder(x, y + 1, maze) == true) return true;
+    if (wayFinder(x - 1, y, maze) == true) return true;
     maze[y][x] = ".";
     return false;
 }
+let maze = [["S", ".", ".","#"],
+            ["#", ".", "#","#"],
+            ["#", ".", "G","#"]];
 
-console.log(wayFinder(0,0,[["S","#","#"],
-                            [".",".","."],
-                            [".","#","G"]]));
+console.log(wayFinder(0, 0, maze));
+console.table(maze);
